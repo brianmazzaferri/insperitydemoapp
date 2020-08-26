3e24af0f441f4580867b6418386043212efcdf01
brianmazzaferri <brianmazzaferri@gmail.com>
2020-08-26 13:49:14 -0500

    check

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ app.command('/prospect', async ({ ack, payload, context }) => {
     const result = await app.client.chat.postMessage({
       token: context.botToken,
       // Channel to send message to
-      channel: context.message.user,
+      channel: payload.channel_id,
       // Include a button in the message (or whatever blocks you want!)
       blocks: [
         {
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: '*New Prospect Alert!* a prospect has submitted <www.oursite.com|documentation> on our portal'
+            text: '*New Prospect Alert!* a prospect has submitted <www.oursite.com|documentation> on our portal\n'+context
           },
           accessory: {
             type: 'button',
